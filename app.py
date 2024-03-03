f618a0ac5f872c0686b2d94d40860fbedd98813e
Shugaba Wuta <shugabawuta@gmail.com>
2024-03-03 19:21:23 +0100

    wip: added person model

--- app.py
+++ app.py
@@ -1,80 +1,83 @@
-'''
+"""
 Flask Application
-'''
+"""
+
 from flask import Flask, jsonify, request
-from models import Experience, Education, Skill
+from models import Experience, Education, Skill, Person
 
 app = Flask(__name__)
 
 data = {
     "experience": [
-        Experience("Software Developer",
-                   "A Cool Company",
-                   "October 2022",
-                   "Present",
-                   "Writing Python Code",
-                   "example-logo.png")
+        Experience(
+            "Software Developer",
+            "A Cool Company",
+            "October 2022",
+            "Present",
+            "Writing Python Code",
+            "example-logo.png",
+        )
     ],
     "education": [
-        Education("Computer Science",
-                  "University of Tech",
-                  "September 2019",
-                  "July 2022",
-                  "80%",
-                  "example-logo.png")
+        Education(
+            "Computer Science",
+            "University of Tech",
+            "September 2019",
+            "July 2022",
+            "80%",
+            "example-logo.png",
+        )
     ],
-    "skill": [
-        Skill("Python",
-              "1-2 Years",
-              "example-logo.png")
-    ]
+    "skill": [Skill("Python", "1-2 Years", "example-logo.png")],
+    "person": [Person("Charles", "+2348109000044", "test.email@example.com")],
 }
 
 
-@app.route('/test')
+@app.route("/test")
 def hello_world():
-    '''
+    """
     Returns a JSON test message
-    '''
+    """
     return jsonify({"message": "Hello, World!"})
 
 
-@app.route('/resume/experience', methods=['GET', 'POST'])
+@app.route("/resume/experience", methods=["GET", "POST"])
 def experience():
-    '''
+    """
     Handle experience requests
-    '''
-    if request.method == 'GET':
+    """
+    if request.method == "GET":
         return jsonify()
 
-    if request.method == 'POST':
+    if request.method == "POST":
         return jsonify({})
 
     return jsonify({})
 
-@app.route('/resume/education', methods=['GET', 'POST'])
+
+@app.route("/resume/education", methods=["GET", "POST"])
 def education():
-    '''
+    """
     Handles education requests
-    '''
-    if request.method == 'GET':
+    """
+    if request.method == "GET":
         return jsonify({})
 
-    if request.method == 'POST':
+    if request.method == "POST":
         return jsonify({})
 
     return jsonify({})
 
 
-@app.route('/resume/skill', methods=['GET', 'POST'])
+@app.route("/resume/skill", methods=["GET", "POST"])
 def skill():
-    '''
+    """
     Handles Skill requests
-    '''
-    if request.method == 'GET':
+    """
+    if request.method == "GET":
         return jsonify({})
 
-    if request.method == 'POST':
+    if request.method == "POST":
         return jsonify({})
 
     return jsonify({})
--- models.py
+++ models.py
@@ -1,17 +1,19 @@
 # pylint: disable=R0913
 
-'''
+"""
 Models for the Resume API. Each class is related to
-'''
-
+"""
+from re import match
 from dataclasses import dataclass
+from phonenumbers import parse, geocoder
 
 
 @dataclass
 class Experience:
-    '''
+    """
     Experience Class
-    '''
+    """
+
     title: str
     company: str
     start_date: str
@@ -22,9 +24,10 @@ class Experience:
 
 @dataclass
 class Education:
-    '''
+    """
     Education Class
-    '''
+    """
+
     course: str
     school: str
     start_date: str
@@ -35,9 +38,31 @@ class Education:
 
 @dataclass
 class Skill:
-    '''
+    """
     Skill Class
-    '''
+    """
+
     name: str
     proficiency: str
     logo: str
+
+
+@dataclass
+class Person:
+    """
+    Person Class
+    """
+
+    name: str
+    phone_number: str
+    email: str
+
+    def get_country_from_phone(self):
+        """Attempts to fetch the country from the phone_number, using the international country code."""
+        parsed_phone_number = parse(self.phone_number)
+        return geocoder.description_for_number(parsed_phone_number, "en")
+
+    def is_email_valid(self):
+        """Returns True if the email provided is valid, otherwise False"""
+        pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+        return match(pattern, self.email) is not None
